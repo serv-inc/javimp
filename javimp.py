5991108f7f48313edce18862a34304a4ac94bbce
serv-inc <serv-inc@users.noreply.github.com>
2018-10-18 12:36:49 +0200

    lacked auto-determining, java class file was not sorted (just `sort filename`)

--- javimp.py
+++ javimp.py
@@ -4,7 +4,7 @@
 # find probable java import statements for a given list of
 # classes specified by root name
 
-import sys, os, fileinput, re, time
+import sys, os, fileinput, re, time, subprocess
 MISSING_3RD_PARTY_MODULES = []
 
 try:
@@ -15,51 +15,29 @@ try:
     import requests
 except ImportError:
     MISSING_3RD_PARTY_MODULES.append("requests")
-try:
-    import pyperclip
-except ImportError:
-    MISSING_3RD_PARTY_MODULES.append("pyperclip")
 
 HELP_MESSAGE_STRING = """
 DESCRIPTION:
-This python script tries to match the arguments you give it to classes in a
-database, and prints out the results it finds. You can update the list of
+
+This python script tries to auto-import the Java files to classes in a
+database, and automatically adds them. You can update the list of
 classes through web scraping by running the program with no arguments.
 
 USAGE:
-python %s [options] [args]
+python  # update database
 or
-python %s [args] [options]
-or
-python %s [args] [options] [more args]
+python %s JavaFile.java [| otherJavaFiles.java]  # auto-import
 
-Please take note that aside from -a mode, only one mode can be in effect
-at the time. Precedence is -i > -o > -c > None
 
 OPTIONS:
     -h: Displays this help message
-    -a: Show all mode; use in combination with any other option to add any
-        additional matches of a class as a commented import
-    -i: Insert mode; insert the complete import statements directly in one or
-        more .java source file(s)
-        Example: Given a source file containing these lines:
-            import Activity;
-            import Bundle;
-        The script would correctly replace this with:
-            import android.app.Activity;
-            import android.os.Bundle;
-    -c: Clipboard mode; copies the correct import statements to your clipboard
-    -o: File output; Creates a file in your current working directory which
-        contains the correct import statements
 
 NOTE: The included java_classes.list file should contain all of the standard
-library classes and the classes for Android app development as of 2016/06/20.
+library classes and the classes for Android app development as of 2018.
 It may be incomplete, contain erroneous information, or be otherwise unsuitable
 for your purposes. This software comes with no warranty or guarantee. Use at
 own risk.
-
-Sincerely, Butterbeard Studios.
-""" %(sys.argv[0],sys.argv[0],sys.argv[0])
+""" %(sys.argv[0])
 
 FILELOCATION = os.path.dirname(os.path.abspath(__file__))
 
@@ -72,15 +50,8 @@ if __name__ == '__main__':
         sys.stderr.write("Warning - 3rd party modules missing: " + ", ".join(MISSING_3RD_PARTY_MODULES) + "\n")
         sys.stderr.write("Some of this program's functionality will not be available.\n\n")
     
-    includeAllMatches = "-a" in sys.argv
-    mode = "i" if "-i" in sys.argv else "o" if "-o" in sys.argv else "c" if "-c" in sys.argv else None
-    
-    if mode == "c":
-        if "pyperclip" in MISSING_3RD_PARTY_MODULES:
-            print("Error - missing modules: pyperclip")
-            sys.exit()
-    
-    pyperclip_str = "" # needed to accumulate string to paste to clipboard
+    includeAllMatches = True
+    mode = "i"
     
     if len(sys.argv) == 1:
         # only python script supplied
@@ -119,9 +90,36 @@ if __name__ == '__main__':
                 # option, we don't wanna deal with this
                 continue
             found = False # used if includeAllMatches
-            if mode == "i":
-                # insert mode is special, so we handle it separately
-                for line in fileinput.input(sys.argv[i], inplace=True):
+            javac = subprocess.Popen(["javac", sys.argv[i]],
+                                     stderr=subprocess.PIPE)
+            (out, err) = javac.communicate()
+            to_import = set()
+            for line in err.split("\n"):
+                if "symbol:" in line:
+                    to_import.add(line.split(" ")[-1])
+
+            # insert mode is special, so we handle it separately
+            for line in fileinput.input(sys.argv[i], inplace=True):
+                while to_import:
+                    current = to_import.pop()
+                    possibles = []
+                    with open(os.path.join(FILELOCATION, "java_classes.list"),
+                              "r") as classlist:
+                        for line in classlist:
+                            if line.endswith("." + current + "\n"):
+                                possibles.append(line.rstrip("\n"))
+                    try:
+                        out = "import {};".format(possibles[0])
+                    except IndexError:
+                        sys.stderr.write("no import found for: " + current)
+                        continue
+                    if possibles[1:]:
+                        out += "  // alternative imports:"
+                        for item in possibles[1:]:
+                            out += " " + item
+                    out += "\n"
+                    sys.stdout.write(out)
+
                     # A NOTE ON FILEINPUT:
                     #
                     # With the inplace option set to True, fileinput backs up
@@ -129,72 +127,23 @@ if __name__ == '__main__':
                     # duration of the loop. Any calls to sys.stdout.write or
                     # print will therefore be written on top of the original
                     # file.
-                    found = False
-                    if line.startswith("import"):
-                        with open(os.path.join(FILELOCATION, "java_classes.list"), "r") as classlist:
-                            for c in classlist:
-                                if c.rstrip().endswith(".%s" %re.sub("import |;", "", line.rstrip())):
-                                    if found:
-                                        # multiple matches; add new matches as comments just in case
-                                        # note how found is set to True AFTER this, so if this is the
-                                        # first class found, it will still be False from earlier
-                                        sys.stdout.write("//")
+                found = False
+                if line.startswith("import"):
+                    with open(os.path.join(FILELOCATION, "java_classes.list"), "r") as classlist:
+                        for c in classlist:
+                            if c.rstrip().endswith(".%s" %re.sub("import |;", "", line.rstrip())):
+                                if found:
+                                    # multiple matches; add new matches as comments just in case
+                                    # note how found is set to True AFTER this, so if this is the
+                                    # first class found, it will still be False from earlier
+                                    sys.stdout.write("//")
                                     
-                                    sys.stdout.write("import %s;\n" %c.rstrip())
-                                    found = True
-                                    if not includeAllMatches:
-                                        break # fuck it, let's just hope it's the right one
-                    if not found:
+                                sys.stdout.write("import %s;\n" %c.rstrip())
+                                found = True
+                                if not includeAllMatches:
+                                    break # fuck it, let's just hope it's the right one
+                if not found:
                         # 1) There was no match for the attempted import
                         # 2) The line didn't start with import, so we jumped straight here
                         # In either case, insert the line as it was
-                        sys.stdout.write(line)
-            else:
-                import_str = ""
-                with open(sys.argv[i], "r") as f:
-                    for line in f:
-                        if line.startswith(" "):
-                            break
-                        if line.startswith("import"):
-                            found = False
-                            with open(os.path.join(FILELOCATION, "java_classes.list"), "r") as classlist:
-                                for c in classlist:
-                                    if c.rstrip().endswith(".%s" %re.sub("import |;", "", line.rstrip())):
-                                        if found:
-                                            # already found, comment out subsequent matches
-                                            import_str += "//"
-                                        # we need to use os.linesep here instead of \n because of
-                                        # clipboard support
-                                        import_str += "import %s;%s" %(c.rstrip(), os.linesep)
-                                        found = True
-                                        if not includeAllMatches:
-                                            break
-                
-                # So import_str now holds a single complete import statement[1]
-                # corresponding to the class specified in sys.argv[i], or is
-                # empty if no match was found[2]. All we need to figure out is
-                # how to output it.
-                #
-                # 1: ... plus an unknown number of subsequent import statements
-                #    that are commented out.
-                # 2: Or if the specified class to import already had the full
-                #    package specified.
-                
-                if mode == "o":
-                    # output to file
-                    with open("import_statements.txt", "a") as o:
-                        o.write(import_str)
-                elif mode == "c":
-                    # we still need to iterate over all the other classes so we can
-                    # copy it all to the clipboard in one go
-                    pyperclip_str += import_str
-                else:
-                    # write to stdout
-                    sys.stdout.write(import_str)
-        
-        if pyperclip_str:
-            # if not -c mode, this was never written to, so it's False
-            # if else, it's either empty because there were no matches,
-            # or it contains all the imports, and we can copy them to
-            # the clipboard
-            pyperclip.copy(pyperclip_str)
+                    sys.stdout.write(line)
